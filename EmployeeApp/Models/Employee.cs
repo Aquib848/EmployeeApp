d853abea76a3617704cd483e4627eadc2c48aa87
Aquib848 <aquibraza958@gmail.com>
2026-04-18 04:05:03 +0530

    Added validation and updated UI for Employee CRUD

--- EmployeeApp/Models/Employee.cs
+++ EmployeeApp/Models/Employee.cs
@@ -5,23 +5,33 @@ public class Employee
 {
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "First Name is required")]
     public string FirstName { get; set; }
 
+    [Required(ErrorMessage = "Last Name is required")]
     public string LastName { get; set; }
 
+    [Required(ErrorMessage = "Date of Birth is required")]
     [DataType(DataType.Date)]
     public DateTime DOB { get; set; }
 
+    [Required(ErrorMessage = "Country is required")]
     public string Country { get; set; }
+
+    [Required(ErrorMessage = "State is required")]
     public string State { get; set; }
+
+    [Required(ErrorMessage = "City is required")]
     public string City { get; set; }
 
+    [Required(ErrorMessage = "Qualification is required")]
     public string Qualification { get; set; }
 
-    [EmailAddress]
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email format")]
     public string Email { get; set; }
 
-    [Phone]
+    [Required(ErrorMessage = "Phone is required")]
+    [Phone(ErrorMessage = "Invalid Phone number")]
     public string Phone { get; set; }
 }
